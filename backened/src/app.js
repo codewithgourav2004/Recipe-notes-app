0404b9136e5585ab9e5f5c5dc2d6b44ab4dd39fc
Gourav <gk154139@gmail.com>
2026-05-29 23:48:24 +0530

    fixed error3

--- backened/src/app.js
+++ backened/src/app.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
 
 const app = express();
 
