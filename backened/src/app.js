d4c09fe9916a650a69a80ec683d6f55c7099a43c
Gourav <gk154139@gmail.com>
2026-05-29 23:54:03 +0530

    fixed error3

--- backened/src/app.js
+++ backened/src/app.js
@@ -119,8 +119,7 @@ app.put("/post/:id", async (req, res) => {
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
-
 module.exports = app;
